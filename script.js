1780d8a7ffd759fc6ab586b839d6669100c3c811
Eduardo Caratachea <120129178+ECaratacheaF22@users.noreply.github.com>
2022-12-13 17:59:09 +0000

    i looks like a gam now

--- script.js
+++ script.js
@@ -1,75 +1,124 @@
 const gameState = {};
 
-function preload(){
+//The Sprites//
+function preload(){ 
   this.load.image('Player', './Invasion/Player.png');
   this.load.image('Ground', './Invasion/Ground_.png');
-  this.load.image('Bullet', './Invasion/Bullet.png');
+  this.load.image('Bullets', './Invasion/Bullet.png');
   this.load.image('UFO2', './Invasion/UFO_2.png');
+  this.load.image('Beams', './Invasion/Beam.png');
 }
+
 
 function create(){ 
-gameState.var, Once = 0
-   gameState.active = true
-
-    this.input.on('pointerup', () => {
-    if (gameState.active === false){
-        this.scene.restart();
-    }
-  });
-  
-    gameState.player = this.physics.add.sprite(300,300,'Player').setScale(.375).setGravityY(200);
-    const platforms = this.physics.add.staticGroup();
-    platforms.create(360,630,'Ground').setScale(1.2,.5).refreshBody();
-
-    gameState.player.setCollideWorldBounds(true);
-   
+//Important stuff//
+gameState.active = true
 gameState.cursors = this.input.keyboard.createCursorKeys();
 
-  this.physics.add.collider(platforms,gameState.player,() => {
-    
-        gameState.active = false
-    this.physics.pause();
-    this.add.text(150,200, 'Game Over Click to Restart',{ fontSize: '25px', fill: '#000'})
-    
-  })
-
-gameState.enemies = this.physics.add.group();
- 
-    for (let y = 1; y < 5; y++){
-        for (let x = 1; x < 2; x++){
-          gameState.enemies.create(600,100*y,'UFO2').setScale(.4);
-        }
-    }
-gameState.Bullet = this.physics.add.group();
-
-  this.physics.add.collider(gameState.enemies,gameState.player,() => {
-        gameState.active = false
-    this.physics.pause();
-    this.add.text(150,200, 'Game Over Click to Restart',{ fontSize: '25px', fill: '#000'})
-    
-  })
-
-    this.physics.add.collider(gameState.enemies,gameState.Bullet,() => {
-
-    
-  })
-
-    this.physics.add.collider(gameState.enemies,gameState.Bullet);
+//Player//
+gameState.player = this.physics.add.sprite(300,300,'Player').setScale(.375).setGravityY(200);
+  gameState.player.setCollideWorldBounds(true);
   
+//Platforms//
+const platforms = this.physics.add.staticGroup();
+platforms.create(360,630,'Ground').setScale(1.2,.5).refreshBody();
+
+//Enemies//
+gameState.Enemies = this.physics.add.group();
+ 
+for (let y = 1; y < 7; y++){
+for (let x = 1; x < 5; x++){
+gameState.Enemies.create(400+75*x,75*y,'UFO2').setScale(.35);
+}}
+
+
+
+  
+//Bullets
+gameState.Bullets = this.physics.add.group();
+const Beams = this.physics.add.group();
+
+    const genBeams = () => {
+        let randomBeams = Phaser.Utils.Array.GetRandom(gameState.Enemies.getChildren());
+        Beams.create(randomBeams.x,randomBeams.y,'Beams').setScale(.1).setVelocityX(-160);
+    }
+
+    gameState.BeamsLoop = this.time.addEvent({
+        delay: 300,
+        callback: genBeams,
+        callbackScope: this,
+        loop: true
+    });
+    this.physics.add.collider(Beams,platforms,
+    (Beams) => {
+      Beams.destroy();
+    });
+
+  this.physics.add.collider(Beams,gameState.player,() => {
+    
+        gameState.active = false
+        gameState.BeamsLoop.destroy();
+    this.physics.pause();
+    this.add.text(150,200, 'Game Over /n Click to Restart',{ fontSize: '25px', fill: '#000'})
+    
+  })
+
+
+
+  
+  
+//Once Var//
+gameState.var, Once = 0
+
+  
+//Restart//
+this.input.on('pointerup', () => {
+  if (gameState.active === false){
+    this.scene.restart();
+}});  
+
+//Colliders//
+this.physics.add.collider(platforms,gameState.player,() => {
+  gameState.active = false
+this.physics.pause();
+this.add.text(150,200, 'Game Over Click to Restart',{ fontSize: '25px', fill: '#000'}) 
+})
+
+this.physics.add.collider(gameState.Enemies,gameState.player,() => {
+  gameState.active = false
+this.physics.pause();
+this.add.text(150,200, 'Game Over Click to Restart',{ fontSize: '25px', fill: '#000'})
+})
+
+  this.physics.add.collider(gameState.Enemies,gameState.Bullets,(enemy, bullet) => {
+    enemy.destroy();
+    bullet.destroy();
+})
+
+
+
+
+
+
+
 }
 function update(){
-/tttttttttttt/
-if (Phaser.Input.Keyboard.JustDown(gameState.cursors.space)) {
 
-  gameState.Bullet.create(gameState.player.x+40, gameState.player.y,'Bullet').setVelocityX(2500).setScale(.25).setGravityY(0);
-}
+
   
+//The Bullet Shoot//
+if (Phaser.Input.Keyboard.JustDown(gameState.cursors.space)) {
+  gameState.Bullets.create(gameState.player.x+40, gameState.player.y,'Bullets').setVelocityX(500).setScale(.25).setGravityY(0);
+}
+
+  //This is so i can only use one movement at a time//
      if (gameState.cursors.up.isDown){
     gameState.Once = 1
     } else {
     gameState.Once = 0
     }
-
+  
+  //Movement//
    if (gameState.cursors.down.isDown){
     gameState.player.setVelocityY(275);
     } else if (gameState.cursors.up.isDown && (gameState.Once = 1)){
@@ -84,6 +133,10 @@ if (Phaser.Input.Keyboard.JustDown(gameState.cursors.space)) {
      gameState.player.setVelocityX(0);
     }
 }
+
+
+
+
 
 const config = {
   type: Phaser.AUTO,
